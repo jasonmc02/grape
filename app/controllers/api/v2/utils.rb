4b43e018a94e036404917e34a71d8888741c8e53
Jason Melendez <jasonmc02@gmail.com>
2016-03-28 21:07:02 -0600

    secondary commit

--- app/controllers/api/v2/utils.rb
+++ app/controllers/api/v2/utils.rb
@@ -4,6 +4,16 @@ module API
 
       version 'v2', using: :path
 
+      helpers do
+        def store_visit
+          Visit.create({ ip: env["REMOTE_ADDR"] })
+        end
+      end
+
+      before do
+        store_visit
+      end
+
       resource :utils do
         desc 'Set cookie.'
         params do
@@ -11,7 +21,7 @@ module API
         end
         post '/set_cookie' do
           cookies[:text] = params[:text]
-          status 200
+          status 201
           { message: 'cookie set' }
         end
 
@@ -20,6 +30,12 @@ module API
           cookie = cookies[:text]
           status 200
           { message: cookie }
+        end
+
+        desc 'Get visits.'
+        get '/get_visits' do
+          visits = Visit.where(:ip => env["REMOTE_ADDR"])
+          { visits_count: visits.size, last_visit: visits.last }
         end
       end
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160326164115) do
+ActiveRecord::Schema.define(version: 20160328211908) do
 
   create_table "countries", force: :cascade do |t|
     t.string   "name",        limit: 255
     t.string   "description", limit: 255
     t.string   "meta",        limit: 255
-    t.datetime "created_at",                              null: false
-    t.datetime "updated_at",                              null: false
-    t.boolean  "allowed",                 default: false
+    t.boolean  "allowed"
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
+
+  create_table "visits", force: :cascade do |t|
+    t.string   "ip",         limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
 end
